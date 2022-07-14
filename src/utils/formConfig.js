504d46d3ee421d0f71c0eb097903a4dcef0c77d2
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-14 11:14:13 +0400

    Create createFormFieldConfig helper function

---
+++ src/utils/formConfig.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Input from '../components/Input';
+
+/**
+ * creates and returns object representation of form field
+ *
+ * @param {string} label - label to show with the form input
+ * @param {string} name - input name
+ * @param {string} type - input type
+ * @param {string} defaultValue - default value for the input
+ */
+function createFormFieldConfig(label, name, type, defaultValue = '') {
+  return {
+    renderInput: (handleChange, value, isValid, error, key) => {
+      return (
+        <Input
+          key={key}
+          name={name}
+          type={type}
+          label={label}
+          isValid={isValid}
+          value={value}
+          handleChange={handleChange}
+          errorMessage={error}
+        />
+      );
+    },
+    label,
+    value: defaultValue,
+    valid: false,
+    errorMessage: '',
+    touched: false,
+  };
+}
+
+export default createFormFieldConfig;
