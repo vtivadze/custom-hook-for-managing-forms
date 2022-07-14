df1e0d6821da07c2d19b6581e022ed0fb2d96584
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-14 11:27:22 +0400

    Refactoring

---
+++ src/components/input/input.js
@@ -0,0 +1,18 @@
+import './input.css';
+
+function InputField(props) {
+  const { label, type, name, handleChange, errorMessage, isValid, value } =
+    props;
+
+  return (
+    <div className='inputContainer'>
+      <label>{label}</label>
+      <input type={type} name={name} value={value} onChange={handleChange} />
+      {errorMessage && !isValid && (
+        <span className='error'>{errorMessage}</span>
+      )}
+    </div>
+  );
+}
+
+export default InputField;
